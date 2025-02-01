8fef299310e9216dcc557d841b443504a36f3be0
qchris101 <qchris101@yahoo.com>
2025-01-31 22:13:44 -0500

    Finished To Do Practice Project

--- app.js
+++ app.js
@@ -8,10 +8,35 @@ const div = document.querySelector("#mainDiv")
 const todoList = document.querySelector("#list")
 // On submit create new Li and add it our ordered list 
 
+// const createDelBttn = () => {
+//     const delBttn = document.createElement("button")
+//     delBttn.textContent = "Delete ToDo"
+//     delBttn.style.marginLeft = "10px"
+//     delBttn.style.backgroundColor = "White"
+//     delBttn.style.borderRadius = "5px"
+//     delBttn.style.color = "black"   
+// }
+
+
 form.addEventListener("submit", function(e) {
     e.preventDefault()
     const newLi = document.createElement("li")
     newLi.textContent = input.value
-    todoList.append(newLi)
+
+    const delBttn = document.createElement("button")
+    delBttn.textContent = "Delete ToDo"
+    delBttn.style.marginLeft = "10px"
+    delBttn.style.backgroundColor = "White"
+    delBttn.style.borderRadius = "5px"
+    delBttn.style.color = "black"   
+
+    delBttn.addEventListener("click", function(e){
+        e.preventDefault()
+        newLi.remove()
+    })
+    newLi.appendChild(delBttn)
+
+    todoList.appendChild(newLi)
+
     input.value = ""
 })
